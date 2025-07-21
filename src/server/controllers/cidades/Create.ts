d5f6a5a22836f34266269f7bf983a9b45bf57baa
mfjaconis <matheusjaconis@gmail.com>
2025-07-21 18:21:03 -0300

    feat/yup: instalando e testando o yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,34 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
   name: string;
+  state: string;
 }
 
 type TypedRequestBody<T> = Request<{}, {}, T>;
 
-export const create = (req: TypedRequestBody<ICidade>, res: Response) => {
-  console.log(req.body);
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  name: yup.string().required().min(3),
+  state: yup.string().required(),
+});
+
+export const create = async (req: TypedRequestBody<ICidade>, res: Response) => {
+  let validateData: ICidade | undefined;
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
+
+  console.log(validateData);
 
   return res.send("Create!");
 };
